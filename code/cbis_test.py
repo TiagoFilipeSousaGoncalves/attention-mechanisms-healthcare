b6365cb51e90bb5cbc78da36fe418a93be28aeda
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-06-11 23:36:50 +0100

    Added new stuff

--- code/cbis_test.py
+++ code/cbis_test.py
@@ -158,44 +158,52 @@ for model_name in MODEL_NAMES:
             y_test_pred = list()
 
 
-            # Load weights and put model in evaluation mode
-            model = model.to(DEVICE)
-            weights_fname = os.path.join(weights_dir, f"{model_name.lower()}_{train_setting}_{weight_split}_cbis.pt")
-            model.load_state_dict(torch.load(weights_fname, map_location=DEVICE))
-            model.eval()
-            
+            try:
+                # Load weights and put model in evaluation mode
+                model = model.to(DEVICE)
+                weights_fname = os.path.join(weights_dir, f"{model_name.lower()}_{train_setting}_{weight_split}_cbis.pt")
+                model.load_state_dict(torch.load(weights_fname, map_location=DEVICE))
+                model.eval()
+                
 
-            # Deactivate gradients
-            with torch.no_grad():
+                # Deactivate gradients
+                with torch.no_grad():
 
-                # Iterate through dataloader
-                for batch_idx, (images, labels) in enumerate(test_loader):
+                    # Iterate through dataloader
+                    for batch_idx, (images, labels) in enumerate(test_loader):
 
-                    # Move data data anda model to GPU (or not)
-                    images, labels = images.to(DEVICE), labels.to(DEVICE)
-                    
+                        # Move data data anda model to GPU (or not)
+                        images, labels = images.to(DEVICE), labels.to(DEVICE)
+                        
 
-                    # Forward pass: compute predicted outputs by passing inputs to the model
-                    logits = model(images)
+                        # Forward pass: compute predicted outputs by passing inputs to the model
+                        logits = model(images)
 
-                    # Concatenate lists
-                    y_test_true += list(labels.cpu().detach().numpy())
+                        # Concatenate lists
+                        y_test_true += list(labels.cpu().detach().numpy())
 
-                    # Using Sigmoid Activation (we apply a threshold of 0.5 in probabilities)
-                    y_test_pred += list(logits.cpu().detach().numpy())
-                    y_test_pred = [1 if i >= 0.5 else 0 for i in y_test_pred]
-
-
-                # Compute Training Accuracy
-                test_acc = accuracy_score(y_true=y_test_true, y_pred=y_test_pred)
-                test_recall = recall_score(y_true=y_test_true, y_pred=y_test_pred)
-                test_precision = precision_score(y_true=y_test_true, y_pred=y_test_pred)
-                test_f1 = f1_score(y_true=y_test_true, y_pred=y_test_pred)
+                        # Using Sigmoid Activation (we apply a threshold of 0.5 in probabilities)
+                        y_test_pred += list(logits.cpu().detach().numpy())
+                        y_test_pred = [1 if i >= 0.5 else 0 for i in y_test_pred]
 
 
-                # Print Statistics
-                print(f'Model Name: {model_name}\tWeight Split: {weight_split}\tTrain Setting: {train_setting}')
-                print(f"Accuracy: {test_acc}\tRecall: {test_recall}\tPrecision: {test_precision}\tF1-Score: {test_f1}")
+                    # Compute Training Accuracy
+                    test_acc = accuracy_score(y_true=y_test_true, y_pred=y_test_pred)
+                    test_recall = recall_score(y_true=y_test_true, y_pred=y_test_pred)
+                    test_precision = precision_score(y_true=y_test_true, y_pred=y_test_pred)
+                    test_f1 = f1_score(y_true=y_test_true, y_pred=y_test_pred)
+
+
+                    # Print Statistics
+                    print(f'Model Name: {model_name} | Weight Split: {weight_split} | Train Setting: {train_setting}')
+                    print(f"Accuracy: {test_acc} | Recall: {test_recall} | Precision: {test_precision} | F1-Score: {test_f1}")
+                    print(f'\n')
+
+            except:
+                print(f'Model Name: {model_name} | Weight Split: {weight_split} | Train Setting: {train_setting}')
+                # print(f"Accuracy: {test_acc} | Recall: {test_recall} | Precision: {test_precision} | F1-Score: {test_f1}")
+                print('Error')
+                print(f'\n')
 
                 
 
