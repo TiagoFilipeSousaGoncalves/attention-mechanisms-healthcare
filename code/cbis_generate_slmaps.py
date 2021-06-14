1b96a497a9f653f961b7e96e00169a5ee7207a77
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-06-14 15:29:20 +0100

    Added Saliency Maps Generation Script for CBIS

--- code/cbis_generate_slmaps.py
+++ code/cbis_generate_slmaps.py
@@ -1,28 +1,19 @@
 # Imports
-# General Imports
 import numpy as np
 import os
-import _pickle as cPickle
-import matplotlib.pyplot as plt
 
-# PyTorch
+# PyTorch Imports
 import torch
 from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch.optim as optim 
-import torch.nn.functional as F
 import torchvision
 
-# Torchsummary
-from torchsummary import summary
-
-# Captum
-from captum.attr import visualization as viz
+# Captum Imports
 from captum.attr import DeepLift
 
 # Project Imports
 from model_utilities import DenseNet121, ResNet50, VGG16, MultiLevelDAM
 from cbis_data_utilities import map_images_and_labels, TorchDatasetFromNumpyArray
+
 
 # To make computations deterministic, let's fix random seeds:
 torch.manual_seed(42)
@@ -35,7 +26,26 @@ def attribute_image_features(model, algorithm, img_input, gt_label, **kwargs):
     model.zero_grad()
     tensor_attributions = algorithm.attribute(img_input, target=gt_label, **kwargs)
     
+
     return tensor_attributions
+
+
+
+# Function to unnormalize images
+def unnormalize(image, mean_array, std_array):
+
+    # Create a copy
+    unnormalized_img = image.copy()
+
+    # Get channels
+    _, _, channels = unnormalized_img.shape
+
+
+    for c in range(channels):
+        unnormalized_img[:, :, c] = image[:, :, c] * std_array[c] + mean_array[c]
+
+
+    return unnormalized_img
 
 
 
@@ -44,6 +54,9 @@ data_dir = "/ctm-hdd-pool01/tgoncalv/datasets/CBIS_proprocessed"
 test_dir = os.path.join(data_dir, "test")
 weights_dir = os.path.join("/ctm-hdd-pool01/tgoncalv/attention-mechanisms-healthcare", "results", "cbis", "weights")
 results_dir = os.path.join("/ctm-hdd-pool01/tgoncalv/attention-mechanisms-healthcare", "results", "cbis")
+attributes_dir = os.path.join(results_dir, "attributes")
+if os.path.isdir(attributes_dir) == False:
+    os.makedirs(attributes_dir)
 
 
 # Choose GPU
@@ -105,95 +118,167 @@ for model_name in MODEL_NAMES:
 
         if model_name == 'DenseNet121':
             model = DenseNet121(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES)
-            mldam_model = MultiLevelDAM(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES, backbone=model_name.lower()
-             pass
+            mldam_model = MultiLevelDAM(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES, backbone=model_name.lower())
+
         
         elif model_name == 'ResNet50':
-            pass
-        
+            model = ResNet50(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES)
+            mldam_model = MultiLevelDAM(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES, backbone=model_name.lower())
+
+
         elif model_name == 'VGG16':
-            pass
+            model = VGG16(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES)
+            mldam_model = MultiLevelDAM(channels=CHANNELS, height=HEIGHT, width=WIDTH, nr_classes=NR_CLASSES, backbone=model_name.lower())
+
+
+
 
 
         # Load models to assess logits
+        # Baseline
         baseline_weights = os.path.join(weights_dir, f"{model_name.lower()}_baseline_val_cbis.pt")
-       
-        
-        
+        model.to(DEVICE)
+        model.load_state_dict(torch.load(baseline_weights, map_location=DEVICE))
+        model.eval()
+
+        # Get prediction
+        baseline_pred = model(images)
+        baseline_pred = 1 if baseline_pred[0].item() >= 0.5 else 0
+
+
+
+        # Baseline Data Augmentation
         baselinedaug_weigths = os.path.join(weights_dir, f"{model_name.lower()}_baselinedaug_val_cbis.pt")
+        model.to(DEVICE)
+        model.load_state_dict(torch.load(baselinedaug_weigths, map_location=DEVICE))
+        model.eval()
+
+        # Get prediction
+        baselinedaug_pred = model(images)
+        baselinedaug_pred = 1 if baselinedaug_pred[0].item() >= 0.5 else 0
+
         
+        # MLDAM
         mldam_weights = os.path.join(weights_dir, f"{model_name.lower()}_mldam_val_cbis.pt")
+        mldam_model.to(DEVICE)
+        mldam_model.load_state_dict(torch.load(mldam_weights, map_location=DEVICE))
+        mldam_model.eval()
+
+        # Get prediction
+        mldam_pred = mldam_model(images)
+        mldam_pred = 1 if mldam_pred[0].item() >= 0.5 else 0
+
+
         
+        # MLDAM w/ Data Augmentation
         mldamdaug_weigths = os.path.join(weights_dir, f"{model_name.lower()}_mldamdaug_val_cbis.pt")
+        mldam_model.to(DEVICE)
+        mldam_model.load_state_dict(torch.load(mldamdaug_weigths, map_location=DEVICE))
+        mldam_model.eval()
 
-        # Forward pass: compute predicted outputs by passing inputs to the model
-        logits = model(images)
-
-        # Concatenate lists
-        y_test_true += list(labels.cpu().detach().numpy())
-
-        # Using Sigmoid Activation (we apply a threshold of 0.5 in probabilities)
-        y_test_pred += list(logits.cpu().detach().numpy())
-        y_test_pred = [1 if i >= 0.5 else 0 for i in y_test_pred]
-
-
-
-
-# Go through test data
-y_true, y_pred = list(), list()
-for batch_idx, batch_data in enumerate(test_loader):
-    print(f"Batch: {batch_idx}")
-    # Get data
-    images, labels = batch_data
-
-    # Get logits
-    logits = model(images)
-    predictions = torch.argmax(logits, dim=1)
-
-    # Original Image
-    for index in range(images.size(0)):
-        original_image = np.transpose((images[index].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
-
-        # Input to the xAI models
-        input_img = images[index].unsqueeze(0)
-        input_img.requires_grad = True
-
-
-        # DeepLift
-        dl = DeepLift(model)
-        attr_dl = attribute_image_features(model, dl, input_img, labels[index])
-        # print(attr_dl.size())
-        attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-
+        # Get prediction
+        mldamdaug_pred = mldam_model(images)
+        mldamdaug_pred = 1 if mldamdaug_pred[0].item() >= 0.5 else 0
 
         
-        # print('Original Image')
-        print('Label: {} | Predicted: {}'.format(labels[index], predictions[index]))
-
-        original_image = np.transpose((images[index].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
-
-        # Show Original Image
-        _ = viz.visualize_image_attr(None, original_image, method="original_image", title="Original Image")
-        save = input(f"Save original image NumPy array (Y/N)? ")
-        if save.lower() == "y":
-            name = input(f"Enter the name for the NumPy array: ")
-            name = name + ".npy"
-            save_path = os.path.join(results_dir, figures_dir, dataset_figs_dir, "Original", name)
-            np.save(file=save_path, arr=original_image, allow_pickle=True)
-
         
+        # Labels
+        label = int(labels[0].item())
 
 
-        # DeepLift
-        _ = viz.visualize_image_attr(attr_dl, None, method="heat_map", sign="all", show_colorbar=True, title="Heatmap DeepLift")
-        _ = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map", sign="all", show_colorbar=True, title="Overlayed DeepLift")
-        save = input(f"Save deeplift attribution NumPy array (Y/N)? ")
-        if save.lower() == "y":
-            name = input(f"Enter the name for the NumPy array: ")
-            name = name + ".npy"
-            save_path = os.path.join(results_dir, figures_dir, dataset_figs_dir, "DeepLift", name)
-            np.save(file=save_path, arr=attr_dl, allow_pickle=True)
+        # Check if these correspond to the correct label
+        if baseline_pred == label and baselinedaug_pred == label and mldam_pred == label and mldamdaug_pred == label:
+            original_image = np.transpose(images[0].cpu().detach().numpy(), (1, 2, 0))
+            original_image = unnormalize(original_image, MEAN, STD)
+
+            # Save image
+            np.save(
+                file=os.path.join(attributes_dir, f"img_{batch_idx}_original_{label}.npy"),
+                arr=original_image,
+                allow_pickle=True
+            )
+
+
+
+            # Input to the xAI models
+            input_img = images[0].unsqueeze(0)
+            input_img.requires_grad = True
+
+
+            # DeepLift - Baseline
+            baseline_weights = os.path.join(weights_dir, f"{model_name.lower()}_baseline_val_cbis.pt")
+            model.to(DEVICE)
+            model.load_state_dict(torch.load(baseline_weights, map_location=DEVICE))
+            model.eval()
+
+            dl_baseline = DeepLift(model)
+            attr_dl_baseline = attribute_image_features(model, dl_baseline, input_img, labels[0])
+            attr_dl_baseline = np.transpose(attr_dl_baseline.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+            # Save attribute
+            np.save(
+                file=os.path.join(attributes_dir, f"img_{batch_idx}_dl_baseline_label_{label}.npy"),
+                arr=attr_dl_baseline,
+                allow_pickle=True
+            )
+
+
+
+            # DeepLift - Baseline Data Augmentation
+            baselinedaug_weigths = os.path.join(weights_dir, f"{model_name.lower()}_baselinedaug_val_cbis.pt")
+            model.to(DEVICE)
+            model.load_state_dict(torch.load(baselinedaug_weigths, map_location=DEVICE))
+            model.eval()
+
+            dl_baselinedaug = DeepLift(model)
+            attr_dl_baselinedaug = attribute_image_features(model, dl_baselinedaug, input_img, labels[0])
+            attr_dl_baselinedaug = np.transpose(attr_dl_baselinedaug.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+            # Save attribute
+            np.save(
+                file=os.path.join(attributes_dir, f"img_{batch_idx}_dl_baselinedaug_label_{label}.npy"),
+                arr=attr_dl_baselinedaug,
+                allow_pickle=True
+            )
+
+
+
+            # MLDAM
+            mldam_weights = os.path.join(weights_dir, f"{model_name.lower()}_mldam_val_cbis.pt")
+            mldam_model.to(DEVICE)
+            mldam_model.load_state_dict(torch.load(mldam_weights, map_location=DEVICE))
+            mldam_model.eval()
+
+            dl_mldam = DeepLift(mldam_model)
+            attr_dl_mldam = attribute_image_features(mldam_model, dl_mldam, input_img, labels[0])
+            attr_dl_mldam = np.transpose(attr_dl_mldam.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+            # Save attribute
+            np.save(
+                file=os.path.join(attributes_dir, f"img_{batch_idx}_dl_mldam_label_{label}.npy"),
+                arr=attr_dl_mldam,
+                allow_pickle=True
+            )
+
+
+
+            # MLDAM w/ Data Augmentation
+            mldamdaug_weigths = os.path.join(weights_dir, f"{model_name.lower()}_mldamdaug_val_cbis.pt")
+            mldam_model.to(DEVICE)
+            mldam_model.load_state_dict(torch.load(mldamdaug_weigths, map_location=DEVICE))
+            mldam_model.eval()
+
+            dl_mldamdaug = DeepLift(mldam_model)
+            attr_dl_mldamdaug = attribute_image_features(mldam_model, dl_mldamdaug, input_img, labels[0])
+            attr_dl_mldamdaug = np.transpose(attr_dl_mldamdaug.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+            # Save attribute
+            np.save(
+                file=os.path.join(attributes_dir, f"img_{batch_idx}_dl_mldamdaug_label_{label}.npy"),
+                arr=attr_dl_mldamdaug,
+                allow_pickle=True
+            )
+            
 
 
 
